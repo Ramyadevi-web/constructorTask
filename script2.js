1e1df773108dd88f30ebf69c73cd48e4b57e828a
Ramyadevi-web <ramyadevim7@gmail.com>
2023-10-26 22:31:18 +0530

    getPG function added

--- script2.js
+++ script2.js
@@ -5,20 +5,22 @@ class Movie{
       this.rating = rating;
       console.log(`${this.title},${this.studio},${this.rating}`);
     }
-    getPG(...a){
-        let pgrate = [];
-     for(let ele of a){
-           if(ele.rating == "PG"){
-            pgrate.push(ele);
-           }
-     }
-     console.log(pgrate);
-    }
+  }
+ function getPG(arr){
+    let pgrate = [];
+ for(let ele of arr){
+       if(ele.rating == "PG"){
+        pgrate.push(ele);
+       }
+ }
+ console.log(pgrate);
 }
-
 const m1 = new Movie("Leo","Seven Screen Studio","PG1");
 const m2 = new Movie("Thuppaki","V Creations","R23");
 const m3 = new Movie("Mersal","Thenandal Studio Limited");
 const m4 = new Movie("Casino Royale","Eon Productions","PG13");
-    getPG(m1,m2,m3,m4);
+
+let arrInstance = [m1,m2,m3,m4];
+
+getPG(arrInstance);
     
